80009f9674f0a14778ff94a01e4e8c797c913011
derlinchen <derlin_nj@163.com>
2018-10-31 14:46:33 +0800

    完整的神经网络样例程序

---
+++ learn/learn08.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+import tensorflow as tf
+from numpy.random import RandomState
+
+batch_size = 8
+
+w1 = tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
+w2 = tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
+
+x = tf.placeholder(tf.float32, shape=(None, 2), name='x-input')
+y_ = tf.placeholder(tf.float32, shape=(None, 1), name='y-input')
+
+a = tf.matmul(x, w1)
+y = tf.matmul(a, w2)
+
+cross_entropy = -tf.reduce_mean(y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)))
+train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+
